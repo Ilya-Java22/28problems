6d4081c263c96246641179d83c6a34ccdcd79b35
Илья <ily470@yandex.ru>
2022-12-01 15:37:08 +0700

    Level1 (вступ. №3: тренировка десантников)

--- src/Level1.java
+++ src/Level1.java
@@ -1,10 +1,45 @@
 public class Level1 {
-    public static int odometer(int [] oksana){
-        int rsl = oksana[0] * oksana[1];
-        for (int i = 2; i < oksana.length; i = i + 2) {
-            int time = oksana[i + 1] - oksana [i - 1];
-            rsl += oksana[i] * time;
+
+    public static int sum2D(int[][] a) {
+        int sum = 0;
+        for(int[] n : a) {
+            for(int m : n) {
+                sum += m;
+            }
         }
-        return rsl;
+        return sum;
     }
+    public static int ConquestCampaign(int N, int M, int L, int [] battalion) {
+        int days = 1;
+        int[][] nm = new int[N][M];
+        for (int i = 0; i < battalion.length; i = i + 2) {
+            nm[battalion[i] - 1][battalion[i + 1] - 1] = 1;
+        }
+        while (sum2D(nm) != N * M) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (nm[i][j] == 1) {
+                        try {
+                            nm[i][j + 1] = 1;
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                        }
+                        try {
+                            nm[i][j - 1] = 1;
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                        }
+                        try {
+                            nm[i + 1][j] = 1;
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                        }
+                        try {
+                            nm[i - 1][j] = 1;
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                        }
+                    }
+                }
+            }
+            days++;
+        }
+        return days;
+        }
 }
