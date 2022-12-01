d118f7814297892fe814586a5aa65e2eff80ee67
Илья <ily470@yandex.ru>
2022-12-01 18:04:22 +0700

    Level1 (вступ. №4: Безумный Макс)

--- src/Level1.java
+++ src/Level1.java
@@ -4,14 +4,12 @@ public class Level1 {
 
     public static int [] MadMax(int N, int [] Tele) {
         Arrays.sort(Tele);
-        int temp = Tele[N / 2];
-        Tele[N / 2] = Tele[N - 1];
-        Tele[N - 1] = temp;
-        for (int i = N / 2 + 1; i < N - 1; i++) {
-            temp = Tele[i];
+        for (int i = N / 2; i < 3 * N / 4; i++) {
+            int temp = Tele[i];
             Tele[i] = Tele[3 * N / 2 - i - 1];
             Tele[3 * N / 2 - i - 1] = temp;
         }
         return Tele;
     }
 }
+
