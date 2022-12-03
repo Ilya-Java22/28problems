1fa4e47839d2465eb10409c75da46731e6e932d8
Илья <ily470@yandex.ru>
2022-12-03 17:56:19 +0700

    Level1 (вступ. №6: Разблокировка мобильных телефонов)

--- src/Level1.java
+++ src/Level1.java
@@ -5,13 +5,13 @@ public class Level1 {
     public static double len(int a, int b) {
         double rsl = 1;
         if (Math.abs(b - a) == 2 || Math.abs(b - a) == 7) {
-            rsl = 1.41421;
+            rsl = Math.sqrt(2);
         }
         if (Math.abs(b - a) == 4 && (b != 3 && b != 7)) {
-            rsl = 1.41421;
+            rsl = Math.sqrt(2);
         }
         if (Math.abs(b - a) == 5 && (b != 1 && b != 6)) {
-            rsl = 1.41421;
+            rsl = Math.sqrt(2);
         }
         return rsl;
     }
@@ -21,7 +21,7 @@ public class Level1 {
         for (int i = 0; i < N - 1; i++) {
             sum += len(hits[i], hits[i + 1]);
         }
-        String a = String.valueOf((int)(sum * 100000));
+        String a = String.valueOf(Math.round(sum * 100000));
         char[] b = new char[a.length()];
         int newSize = 0;
         int i = 0;
