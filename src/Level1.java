d75ddfe7e4773683cfdf98c5149d370e1615036d
Илья <ily470@yandex.ru>
2022-12-06 20:29:41 +0700

    Level1 (№8: Искусственный интеллект для Оксаны )

--- src/Level1.java
+++ src/Level1.java
@@ -2,42 +2,19 @@ import java.util.*;
 
 public class Level1 {
 
-    public static List<String> xxx(int len, String s) {
-        List<String> buffer = new ArrayList<>();
-        while (s.length() > len) {
-            if (s.charAt(len - 1) == ' ') {
-                buffer.add(s.substring(0, len - 1));
-                s = s.substring(len);
-            } else if (s.charAt(len) == ' ') {
-                buffer.add(s.substring(0, len));
-                s = s.substring(len + 1);
-            } else if (s.charAt(len) != ' ') {
-                boolean k = true;
-                for (int i = len - 2; i > 0; i--) {
-                    if (s.charAt(i) == ' ') {
-                        buffer.add(s.substring(0, i));
-                        s = s.substring(i + 1);
-                        k = false;
-                        break;
-                    }
-                }
-                if (k) {
-                    buffer.add(s.substring(0, len));
-                    s = s.substring(len);
+    public static int SumOfThe(int N, int [] data)  {
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i != j) {
+                    sum += data[j];
                 }
             }
+            if (data[i] == sum) {
+                break;
+            }
+            sum = 0;
         }
-        buffer.add(s);
-        buffer.removeAll(Arrays.asList(""));
-        return buffer;
-    }
-
-    public static int [] WordSearch(int len, String s, String subs)  {
-        ArrayList<Integer> rsl = new ArrayList<>();
-        List<String> strings = xxx(len, s);
-        for (String x : strings) {
-            rsl.add(x.matches("(.*)"+ "(\s|^)" + subs+ "(\s|$)" + "(.*)") ? 1 : 0);
-        }
-        return rsl.stream().mapToInt(i -> i).toArray();
+        return sum;
     }
 }
