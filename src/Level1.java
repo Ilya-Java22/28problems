b1f49f82a61100c5b9c1842a5ed4994c3fa871df
Илья <ily470@yandex.ru>
2022-12-06 15:35:19 +0700

    Level1 (№7: Конкурент Google)

--- src/Level1.java
+++ src/Level1.java
@@ -4,40 +4,40 @@ public class Level1 {
 
     public static List<String> xxx(int len, String s) {
         List<String> buffer = new ArrayList<>();
-        if (s.length() <= len) {
-            buffer.add(s);
-            return buffer;
-        }
         while (s.length() > len) {
-            for (int i = len - 1; i >= 0; i--) {
-                if (s.charAt(i) == ' ' && i > 0) {
-                    buffer.add(s.substring(0, i));
-                    s = s.substring(s.charAt(i + 1) != ' ' ? i + 1 : i + 2);
-                    i = len;
-                    if (s.length() <= len) {
-                        buffer.add(s);
-                        return buffer;
+            if (s.charAt(len - 1) == ' ') {
+                buffer.add(s.substring(0, len - 1));
+                s = s.substring(len);
+            } else if (s.charAt(len) == ' ') {
+                buffer.add(s.substring(0, len));
+                s = s.substring(len + 1);
+            } else if (s.charAt(len) != ' ') {
+                boolean k = true;
+                for (int i = len - 2; i > 0; i--) {
+                    if (s.charAt(i) == ' ') {
+                        buffer.add(s.substring(0, i));
+                        s = s.substring(i + 1);
+                        k = false;
+                        break;
                     }
                 }
-            }
-            buffer.add(s.substring(0, len));
-            s = s.substring(s.charAt(len) != ' ' ? len : len + 1);
-            if (s.length() <= len && s.length() != 0 && !" ".equals(s)) {
-                buffer.add(s);
-                return buffer;
+                if (k) {
+                    buffer.add(s.substring(0, len));
+                    s = s.substring(len);
+                }
             }
         }
+        buffer.add(s);
+        buffer.removeAll(Arrays.asList(""));
         return buffer;
     }
 
     public static int [] WordSearch(int len, String s, String subs)  {
         ArrayList<Integer> rsl = new ArrayList<>();
         List<String> strings = xxx(len, s);
-        System.out.println(strings);
         for (String x : strings) {
             rsl.add(x.matches("(.*)"+ "(\s|^)" + subs+ "(\s|$)" + "(.*)") ? 1 : 0);
-            }
+        }
         return rsl.stream().mapToInt(i -> i).toArray();
     }
 }
-
