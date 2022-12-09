9eba1531e464dc1c956c95d8a4c8dfacd6895c56
Илья <ily470@yandex.ru>
2022-12-09 15:45:19 +0700

    Level1 (№9: Миссия невыполнима: Красный портфель )

--- src/Level1.java
+++ src/Level1.java
@@ -2,19 +2,38 @@ import java.util.*;
 
 public class Level1 {
 
-    public static int SumOfThe(int N, int [] data)  {
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i != j) {
-                    sum += data[j];
+    public static String TheRabbitsFoot(String s, boolean encode)  {
+        StringBuilder buffer = new StringBuilder();
+        for (char x : s.toCharArray()) {
+            if (x != ' ') {
+                buffer.append(x);
+            }
+        }
+        int n = (int) Math.sqrt(buffer.length());
+        int m = (int) Math.ceil(Math.sqrt(buffer.length()));
+        if (n * m < buffer.length()) {
+            n++;
+        }
+        if (!encode) {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+        char[][] nm = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            buffer.getChars(m * i, Math.min(m * (i + 1), buffer.length()), nm[i],0);
+        }
+        StringBuilder buffer2 = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (nm[j][i] != 0) {
+                    buffer2.append(nm[j][i]);
                 }
             }
-            if (data[i] == sum) {
-                break;
+            if (encode) {
+                buffer2.append(" ");
             }
-            sum = 0;
         }
-        return sum;
+        return buffer2.toString();
     }
 }
