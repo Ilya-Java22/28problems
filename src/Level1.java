81e3fe793755d7bbf12b09790ce4381ede88ed53
Илья <ily470@yandex.ru>
2022-12-13 15:40:32 +0700

    Level1 (№12: "146%")

--- src/Level1.java
+++ src/Level1.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 public class Level1 {
 
-    public static String BigMinus(String s1, String s2) {
-        if (s2.length() > s1.length() || s1.length() == s2.length() && s2.charAt(0) > s1.charAt(0)) {
-            String temp = s1;
-            s1 = s2;
-            s2 = temp;
+    public static String MassVote(int N, int[] Votes) {
+        if (N == 1) {
+            return "majority winner 1";
         }
-        StringBuilder rsl = new StringBuilder();
-        StringBuilder minus = new StringBuilder(s2);
-        for (int i = 0; i < s1.length() - s2.length(); i++) {
-            minus.insert(0, 0);
+        int[] origin = Arrays.copyOf(Votes, N);
+        Arrays.sort(Votes);
+        if (Votes[N - 1] == Votes[N - 2]) {
+            return "no winner";
         }
-        for (int i = s1.length() - 1, k = 0; i >= 0 ; i--) {
-            int a = s1.charAt(i) - 48 - k;
-            k = 0;
-            int b = minus.charAt(i) - 48;
-            if (a < b) {
-                a += 10;
-                k = 1;
-            }
-            rsl.insert(0, a - b);
-        }
-        for(int i = 0; i < rsl.length() - 1; i++) {
-            if (rsl.charAt(i) != 48) {
-                return rsl.substring(i);
+        int index = 0;
+        for (int i = 0; i < N; i++) {
+            if (origin[i] == Votes[N - 1]) {
+                index = i;
+                break;
             }
         }
-        return rsl.substring(rsl.length() - 1);
+        int sum = 0;
+        for (int x : Votes) {
+            sum += x;
+        }
+        if (Math.round(100000.0 * Votes[N - 1] / sum) > 50000) {
+            return "majority winner " + (index + 1);
+        } else {
+            return "minority winner " + (index + 1);
+        }
     }
 }
