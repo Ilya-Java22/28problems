76bf8257db68155b318ac77a27c9db5fb83f5a63
Илья <ily470@yandex.ru>
2022-12-16 02:11:20 +0700

    Level1 (№13: "Сигналы НЛО")

--- src/Level1.java
+++ src/Level1.java
@@ -1,31 +1,13 @@
-import java.util.*;
 
 public class Level1 {
-
-    public static String MassVote(int N, int[] Votes) {
-        if (N == 1) {
-            return "majority winner 1";
-        }
-        int[] origin = Arrays.copyOf(Votes, N);
-        Arrays.sort(Votes);
-        if (Votes[N - 1] == Votes[N - 2]) {
-            return "no winner";
-        }
-        int index = 0;
-        for (int i = 0; i < N; i++) {
-            if (origin[i] == Votes[N - 1]) {
-                index = i;
-                break;
-            }
-        }
-        int sum = 0;
-        for (int x : Votes) {
-            sum += x;
-        }
-        if (Math.round(100000.0 * Votes[N - 1] / sum) > 50000) {
-            return "majority winner " + (index + 1);
-        } else {
-            return "minority winner " + (index + 1);
-        }
+    public static int[] UFO(int N, int[] data, boolean octal) {
+       int[] rsl = new int[N];
+       String s;
+       int radix = octal ? 8 : 16;
+       for (int i = 0; i < N; i++) {
+           s = String.valueOf(data[i]);
+           rsl[i] = Integer.parseInt(s, radix);
+       }
+       return rsl;
     }
 }
