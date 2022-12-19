74cd8c6ec53ed116b7af9cc5d5ac6e6ebb97b8f6
Илья <ily470@yandex.ru>
2022-12-19 18:34:08 +0700

    Level1 (№14: "Оптимизация беспилотного трафика")

--- src/Level1.java
+++ src/Level1.java
@@ -3,6 +3,9 @@ public class Level1 {
     public static int Unmanned (int L, int N, int[][] track) {
        int delta = 0;
        for (int i = 0; i < N; i++) {
+           if (track[i][0] >= L) {
+               break;
+           }
            int x = (delta + track[i][0]) % (track[i][1] + track[i][2]);
            delta += x < track[i][1] ? track[i][1] - x : 0;
        }
