ae4806f7785db9eb7f3b4b2842a6222f028c4b6e
Илья <ily470@yandex.ru>
2022-12-19 17:15:07 +0700

    Level1 (№14: "Оптимизация беспилотного трафика")

--- src/Level1.java
+++ src/Level1.java
@@ -1,13 +1,11 @@
 
 public class Level1 {
-    public static int[] UFO(int N, int[] data, boolean octal) {
-       int[] rsl = new int[N];
-       String s;
-       int radix = octal ? 8 : 16;
+    public static int Unmanned (int L, int N, int[][] track) {
+       int delta = 0;
        for (int i = 0; i < N; i++) {
-           s = String.valueOf(data[i]);
-           rsl[i] = Integer.parseInt(s, radix);
+           int x = (delta + track[i][0]) % (track[i][1] + track[i][2]);
+           delta += x < track[i][1] ? track[i][1] - x : 0;
        }
-       return rsl;
+       return L + delta;
     }
 }
