c9048c9ab626c8313e0a5f92d2eafc152f8d7f26
Илья <ily470@yandex.ru>
2022-12-20 16:23:33 +0700

    Level1 (№16: "Шопоголики")

--- src/Level1.java
+++ src/Level1.java
@@ -1,37 +1,12 @@
+import java.util.*;
 
 public class Level1 {
-    public static char[][] fillArray(int h, int w, String s) {
-        String[] a = s.split(" ");
-        char[][] rsl = new char[h][w];
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                rsl[i][j] = a[i].charAt(j);
-            }
+    public static int MaximumDiscount(int N, int [] price) {
+        int sum = 0;
+        Arrays.sort(price);
+        for (int i = N - 3; i >= 0; i -= 3) {
+            sum += price[i];
         }
-        return rsl;
-    }
-
-    public static boolean equalArray(int x, int y, char[][] a, char[][] b) {
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < b[0].length; j++) {
-                if (a[i + x][j + y] != b[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
-        char[][] a = fillArray(H1, W1, S1);
-        char[][] b = fillArray(H2, W2, S2);
-        for (int i = 0; i <= H1 - H2; i++) {
-            for (int j = 0; j <= W1 - W2; j++) {
-                if (a[i][j] == b[0][0] && equalArray(i, j, a, b)) {
-                    return true;
-                }
-            }
-        }
-       return false;
+        return sum;
     }
 }
