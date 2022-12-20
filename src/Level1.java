c2b238bd2f627652d961be36b2355daebfa446d2
Илья <ily470@yandex.ru>
2022-12-20 15:38:32 +0700

    Level1 (№15: "Танковый раш")

--- src/Level1.java
+++ src/Level1.java
@@ -1,14 +1,37 @@
 
 public class Level1 {
-    public static int Unmanned (int L, int N, int[][] track) {
-       int delta = 0;
-       for (int i = 0; i < N; i++) {
-           if (track[i][0] >= L) {
-               break;
-           }
-           int x = (delta + track[i][0]) % (track[i][1] + track[i][2]);
-           delta += x < track[i][1] ? track[i][1] - x : 0;
-       }
-       return L + delta;
+    public static char[][] fillArray(int h, int w, String s) {
+        String[] a = s.split(" ");
+        char[][] rsl = new char[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                rsl[i][j] = a[i].charAt(j);
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean equalArray(int x, int y, char[][] a, char[][] b) {
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                if (a[i + x][j + y] != b[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
+        char[][] a = fillArray(H1, W1, S1);
+        char[][] b = fillArray(H2, W2, S2);
+        for (int i = 0; i <= H1 - H2; i++) {
+            for (int j = 0; j <= W1 - W2; j++) {
+                if (a[i][j] == b[0][0] && equalArray(i, j, a, b)) {
+                    return true;
+                }
+            }
+        }
+       return false;
     }
 }
