d807110efb43da4839055f871f46301c816f1a73
Илья <ily470@yandex.ru>
2022-12-24 14:24:42 +0700

    Level1 (№18: "Мистер Робот и Корпорация Зла ")

--- src/Level1.java
+++ src/Level1.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
-
 public class Level1 {
     public static boolean MisterRobot(int N, int[] data) {
         boolean x = true;
+        int n = 0;
         while (x) {
             x = false;
+            n = 0;
             for (int i = 1, temp = 0; i < N - 1; i++) {
                 if (data[i] >= data[i - 1] && data[i + 1] < data[i - 1]
                         || data[i + 1] == data[i - 1] && data[i] > data[i - 1]) {
@@ -22,14 +22,13 @@ public class Level1 {
                     data[i] = data[i + 1];
                     data[i + 1] = temp;
                     x = true;
+                    continue;
+                }
+                if (data[i + 1] >= data[i] && data[i - 1] <= data[i]) {
+                    n++;
                 }
             }
         }
-        for (int i = 0; i < N - 1; i++) {
-            if (data[i] > data[i + 1]) {
-                return false;
-            }
-        }
-        return true;
+        return n == N - 2;
     }
 }
