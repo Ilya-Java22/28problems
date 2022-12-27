a2dd9c106ace56f43a60733673b54242d2b5b876
Илья <ily470@yandex.ru>
2022-12-28 02:58:48 +0700

    Level1 (№19: "Автоматизация отчётности о продажах ")

--- src/Level1.java
+++ src/Level1.java
@@ -1,34 +1,23 @@
+import java.util.*;
+
 public class Level1 {
-    public static boolean MisterRobot(int N, int[] data) {
-        boolean x = true;
-        int n = 0;
-        while (x) {
-            x = false;
-            n = 0;
-            for (int i = 1, temp = 0; i < N - 1; i++) {
-                if (data[i] >= data[i - 1] && data[i + 1] < data[i - 1]
-                        || data[i + 1] == data[i - 1] && data[i] > data[i - 1]) {
-                    temp = data[i + 1];
-                    data[i + 1] = data[i];
-                    data[i] = data[i - 1];
-                    data[i - 1] = temp;
-                    x = true;
-                    continue;
-                }
-                if (data[i + 1] <= data[i - 1] && data[i + 1] > data[i]
-                        || data[i + 1] == data[i] && data[i - 1] > data[i]) {
-                    temp = data[i - 1];
-                    data[i - 1] = data[i];
-                    data[i] = data[i + 1];
-                    data[i + 1] = temp;
-                    x = true;
-                    continue;
-                }
-                if (data[i + 1] >= data[i] && data[i - 1] <= data[i]) {
-                    n++;
-                }
-            }
+    public static String[] ShopOLAP(int N, String[] items)  {
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String s : items) {
+            String[] ss = s.split(" ");
+            hm.put(ss[0], hm.getOrDefault(ss[0], 0) + Integer.parseInt(ss[1]));
         }
-        return n == N - 2;
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(hm.entrySet());
+        Comparator<Map.Entry<String, Integer>> comp = (o1, o2) -> {
+            int rsl =  o1.getValue().compareTo(o2.getValue());
+            return rsl == 0 ? o1.getKey().compareTo(o2.getKey()) : rsl;
+        };
+        entries.sort(comp);
+        String[] result = new String[entries.size()];
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : entries) {
+            result[i++] = entry.getKey() + " " + entry.getValue();
+        }
+        return result;
     }
 }
