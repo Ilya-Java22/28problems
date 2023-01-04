a452cebf7be405ff4284a7c058a92862e0f8bf82
Илья <ily470@yandex.ru>
2023-01-04 20:39:03 +0700

    Level1 (№20: "Делаем национальный редактор "Лапоть" ")

--- src/Level1.java
+++ src/Level1.java
@@ -1,23 +1,54 @@
 import java.util.*;
 
 public class Level1 {
-    public static String[] ShopOLAP(int N, String[] items)  {
-        HashMap<String, Integer> hm = new HashMap<>();
-        for (String s : items) {
-            String[] ss = s.split(" ");
-            hm.put(ss[0], hm.getOrDefault(ss[0], 0) + Integer.parseInt(ss[1]));
+    static Stack<String> undo = new Stack<>();
+    static Stack<String> redo = new Stack<>();
+    static boolean action = false;
+
+    public static String BastShoe(String command) {
+        String[] s1 = command.split(" ", 2);
+        switch (Integer.parseInt(s1[0])) {
+            case 1 :
+                if (action) {
+                    redo.clear();
+                    undo.subList(0, undo.size() - 1).clear();
+                    action = false;
+                }
+                String s2 = undo.isEmpty() ? "" : undo.peek();
+                undo.push(String.join("", s2, s1[1]));
+                return undo.peek();
+            case 2 :
+                if (action) {
+                    redo.clear();
+                    undo.subList(0, undo.size() - 1).clear();
+                    action = false;
+                }
+                String s3 = undo.isEmpty() ? "" : undo.peek();
+                int ch = Integer.parseInt(s1[1]);
+                ch = Math.min(ch, s3.length());
+                undo.push(s3.substring(0, s3.length() - ch));
+                return undo.peek();
+            case 3 :
+                String s4 = undo.isEmpty() ? "" : undo.peek();
+                int n = Integer.parseInt(s1[1]);
+                if (n >= s4.length()) {
+                    return "";
+                }
+                return String.valueOf(s4.charAt(n));
+            case 4 :
+                action = true;
+                if (undo.size() != 1) {
+                    redo.push(undo.pop());
+                }
+                return undo.peek();
+            case 5 :
+                action = false;
+                if (!redo.isEmpty()) {
+                    undo.push(redo.pop());
+                }
+                return undo.peek();
+            default :
+                return undo.isEmpty() ? "" : undo.peek();
         }
-        List<Map.Entry<String, Integer>> entries = new ArrayList<>(hm.entrySet());
-        Comparator<Map.Entry<String, Integer>> comp = (o1, o2) -> {
-            int rsl =  o2.getValue().compareTo(o1.getValue());
-            return rsl == 0 ? o1.getKey().compareTo(o2.getKey()) : rsl;
-        };
-        entries.sort(comp);
-        String[] result = new String[entries.size()];
-        int i = 0;
-        for (Map.Entry<String, Integer> entry : entries) {
-            result[i++] = entry.getKey() + " " + entry.getValue();
-        }
-        return result;
     }
 }
