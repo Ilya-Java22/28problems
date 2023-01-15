911ce63db81a273099019aa22c7af8177132f6d3
Илья <ily470@yandex.ru>
2023-01-16 01:38:56 +0700

    Level1 (№25: "Чубакка и гравитационные колодцы")

--- src/Level1.java
+++ src/Level1.java
@@ -1,54 +1,24 @@
+import java.util.*;
+
 public class Level1 {
 
-    public static StringBuilder[] stringToStringBuilder(String[] Matrix, int M, int N) {
-        StringBuilder[] sb = new StringBuilder[Math.min(N, M) / 2];
-        for (int k = 0; k < sb.length; k++) {
-            sb[k] = new StringBuilder();
-            for (int i = k; i < N - k; i++) {
-                sb[k].append(Matrix[k].charAt(i));
-            }
-            for (int i = 1 + k; i < M - 1 - k; i++) {
-                sb[k].append(Matrix[i].charAt(N - 1 - k));
-            }
-            for (int i = k; i < N - k; i++) {
-                sb[k].append(Matrix[M - 1 - k].charAt(N - 1 - i));
-            }
-            for (int i = 1 + k; i < M - 1 - k; i++) {
-                sb[k].append(Matrix[M - 1 - i].charAt(k));
+    public static int[] s(int[] A, int N) {
+        LinkedList<Integer> B = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                int k = i + j;
+                int[] ar = Arrays.copyOfRange(A, j, k + 1);
+                Arrays.sort(ar);
+                B.addLast(ar[ar.length - 1]);
             }
         }
-        return sb;
+        return B.stream().mapToInt(i -> i).toArray();
     }
 
-    public static char[][] stringBuilderToCharArray(StringBuilder[] sb, int M, int N) {
-        char[][] ch = new char[M][N];
-        for (int k = 0; k < sb.length; k++) {
-            int j = 0;
-            for (int i = k; i < N - k; i++) {
-                ch[k][i] = sb[k].charAt(j++);
-            }
-            for (int i = 1 + k; i < M - 1 - k; i++) {
-                ch[i][N - 1 - k] = sb[k].charAt(j++);
-            }
-            for (int i = k; i < N - k; i++) {
-                ch[M - 1 - k][N - 1 - i] = sb[k].charAt(j++);
-            }
-            for (int i = 1 + k; i < M - 1 - k; i++) {
-                ch[M - 1 - k - i][k] = sb[k].charAt(j++);
-            }
-        }
-        return ch;
-    }
-
-    public static void MatrixTurn(String[] Matrix, int M, int N, int T) {
-        StringBuilder[] sb = stringToStringBuilder(Matrix,  M,  N);
-        for (StringBuilder s : sb) {
-            s.insert(0, s.substring(s.length() - T));
-            s.delete(s.length() - T, s.length());
-        }
-        char[][] ch = stringBuilderToCharArray(sb, M, N);
-        for (int i = 0; i < M; i++) {
-            Matrix[i] = String.valueOf(ch[i]);
-        }
+    public static boolean TransformTransform(int[] A, int N) {
+        int[] arr = s(A, N);
+        int[] arr2 = s(arr, arr.length);
+        int sum = Arrays.stream(A).sum();
+        return sum % 2 == 0;
     }
 }
