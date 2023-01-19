32c3b56cc036113a66e5efb52efa7ec06778c837
Илья <ily470@yandex.ru>
2023-01-19 22:37:36 +0700

    Level1 (№27: "Тренируем сборную России по футболу")

--- src/Level1.java
+++ src/Level1.java
@@ -1,25 +1,49 @@
 import java.util.*;
-import static java.lang.Character.isDigit;
 
 public class Level1 {
 
-    public static boolean white_walkers(String village) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < village.length(); i++) {
-            if (isDigit(village.charAt(i))) {
-                list.add(i);
+    public static boolean checkSort(int[] F) {
+        int[] copyF = F.clone();
+        Arrays.sort(F);
+        return Arrays.equals(copyF, F);
+    }
+
+    public static boolean Football(int[] F, int N) {
+        int[] copyF = F.clone();
+        int a = -1;
+        int b = 0;
+        for (int i = 0; i < N - 1; i++) {
+            if (a != -1 && copyF[i] < copyF[i + 1]) {
+                break;
+            }
+            if (copyF[i] > copyF[i + 1]) {
+                a = a < 0 ? i : a;
+                b = i + 1;
             }
         }
-        if (list.size() < 2) {
-            return false;
+        Arrays.sort(copyF, a, b + 1);
+        if (b - a > 1) {
+            return checkSort(copyF);
         }
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (village.charAt(list.get(i)) + village.charAt(list.get(i + 1)) == 106
-                    && !village.substring(list.get(i) + 1, list.get(i + 1)).matches("(\\D*)"
-                    + "=" + "(\\D*)" + "=" + "(\\D*)" + "=" + "(\\D*)")) {
+        if (b - a == 1 && checkSort(copyF)) {
+            return true;
+        }
+        int submax = copyF[b];
+        int submin = copyF[a];
+        for (int j = b + 1; j < N; j++) {
+            copyF = F.clone();
+            if (copyF[j] > submax) {
                 return false;
             }
+            if (copyF[j] < submin) {
+                int temp = copyF[a];
+                copyF[a] = copyF[j];
+                copyF[j] = temp;
+                if (checkSort(copyF)) {
+                    return true;
+                }
+            }
         }
-        return true;
+        return false;
     }
 }
