04f6b2d065de2ec5da19c4c8fb6a8bccdf1b780b
Илья <ily470@yandex.ru>
2023-01-20 20:30:24 +0700

    Level1 (№28: "Мастер ключей")

--- src/Level1.java
+++ src/Level1.java
@@ -1,52 +1,13 @@
-import java.util.*;
-
 public class Level1 {
-
-    public static boolean checkSort(int[] F) {
-        int[] copyF = F.clone();
-        Arrays.sort(F);
-        return Arrays.equals(copyF, F);
-    }
-
-    public static boolean Football(int[] F, int N) {
-        int[] copyF = F.clone();
-        int a = -1;
-        int b = 0;
-        for (int i = 0; i < N - 1; i++) {
-            if (a != -1 && copyF[i] < copyF[i + 1]) {
-                break;
-            }
-            if (copyF[i] > copyF[i + 1]) {
-                a = a < 0 ? i : a;
-                b = i + 1;
-            }
-        }
-        if (a == -1) {
-            return false;
-        }
-        Arrays.sort(copyF, a, b + 1);
-        if (b - a > 1) {
-            return checkSort(copyF);
-        }
-        if (b - a == 1 && checkSort(copyF)) {
-            return true;
-        }
-        int submax = copyF[b];
-        int submin = copyF[a];
-        for (int j = b + 1; j < N; j++) {
-            copyF = F.clone();
-            if (copyF[j] > submax) {
-                return false;
-            }
-            if (copyF[j] < submin) {
-                int temp = copyF[a];
-                copyF[a] = copyF[j];
-                copyF[j] = temp;
-                if (checkSort(copyF)) {
-                    return true;
+        public static String Keymaker(int k) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < k; i++) {
+                if (Math.sqrt(i + 1) == (int)Math.sqrt(i + 1)) {
+                    sb.append(1);
+                } else {
+                    sb.append(0);
                 }
             }
+            return sb.toString();
         }
-        return false;
-    }
 }
